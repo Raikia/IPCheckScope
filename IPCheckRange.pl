0429a67a13ed8ec75378baee7bcd928d80f97446
Chris King <raikiasec@gmail.com>
2014-11-04 14:32:13 -0800

    Initial commit

---
+++ IPCheckRange.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+use Net::Subnet;
+use Getopt::Long;
+use Pod::Usage;
+
+my $ranges = '';
+my $listIPs = '';
+my $list_in_range = 0;
+my $list_out_range = 0;
+
+GetOptions("range=s" => \$ranges,
+	   "file=s" => \$listIPs,
+   	   "in-range" => \$list_in_range,
+   	   "out-range" => \$list_out_range) or die "Error in command line arguments\n";
+
+unless ($ranges and $listIPs and ($list_in_range xor $list_out_range)) {
+	pod2usage(1);
+	exit;
+}
+
+open(RANGES, $ranges) or die "Cannot open file: $!";
+
+my @ranges = <RANGES>;
+chomp foreach (@ranges);
+my $matcher = subnet_matcher @ranges;
+
+close(RANGES);
+open(IPS, $listIPs) or die "Cannot open file: $!";
+
+while (<IPS>) {
+	chomp;
+	if ($list_in_range) {
+		if ($matcher->($_)) {
+			print "$_\n";
+		}
+	}
+	elsif ($list_out_range) {
+		unless ($matcher->($_)) {
+			print "$_\n";
+		}
+	}
+}
+
+close(IPS);
+
+__END__
+=head1 IPCheckRange.pl
+
+Simple script to check if a list of IPs is within the specified range
+
+=head1 SYNOPSIS
+
+./IPCheckRange.pl [options]
+
+  Options:
+    --range, -r			File of IP ranges, either in CIDR or in range format
+    				Ex: 127.0.0.1/24  or  127.0.0.1-50
+
+    --file, -f			File of IP addresses to test
+
+    --in-range			Display IPs that are within specified ranges
+				(Cannot be used with --out-range
+
+    --out-range			Display IPs that are not within specified ranges
+				(Cannot be used with --in-range)
+
+
+Requires:
+	Net::Subnet
+
+
